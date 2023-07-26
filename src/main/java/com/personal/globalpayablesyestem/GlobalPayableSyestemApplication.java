55b7258e66157a3b25d42b2db1974c69c6458c1f
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-07-26 15:31:14 +0600

    first commit

---
+++ src/main/java/com/personal/globalpayablesyestem/GlobalPayableSyestemApplication.java
@@ -0,0 +1,13 @@
+package com.personal.globalpayablesyestem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalPayableSyestemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalPayableSyestemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/globalpayablesyestem/GlobalPayableSyestemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.globalpayablesyestem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalPayableSyestemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
