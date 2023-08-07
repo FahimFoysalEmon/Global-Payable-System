58f63bc991118f6d6c82c63f5e36eaafbde0c3c0
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-07 16:05:48 +0600

    UserDetails service implemented

--- src/main/java/com/personal/globalpayablesyestem/user/User.java
+++ src/main/java/com/personal/globalpayablesyestem/user/User.java
@@ -6,14 +6,18 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Data
 @Table(name = "user_entity")
-public class User{
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(generator = "system-uuid")
@@ -33,4 +37,39 @@ public class User{
     @UpdateTimestamp
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+05:30")
     private LocalDateTime lastUpdatedOn;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
