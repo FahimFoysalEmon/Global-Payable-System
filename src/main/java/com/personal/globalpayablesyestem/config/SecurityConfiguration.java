260d5900feac5ee695d0182df8cbadd2ddccbb8b
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-20 16:47:55 +0600

    Security Configuration added

---
+++ src/main/java/com/personal/globalpayablesyestem/config/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.personal.globalpayablesyestem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+}
