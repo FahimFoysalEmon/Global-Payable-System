b4df8c00b8327dcb76a495419457f64e8ad5fb6c
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-22 18:04:45 +0600

    bank controller -> get bank demo api

--- src/main/java/com/personal/globalpayablesyestem/Bank/BankController.java
+++ src/main/java/com/personal/globalpayablesyestem/Bank/BankController.java
@@ -1,4 +1,19 @@
 package com.personal.globalpayablesyestem.Bank;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/private")
 public class BankController {
+
+    @GetMapping(value = "/banks")
+    public ResponseEntity<String> getBank() {
+        return ResponseEntity.ok("Demo all banks");
+    }
+
 }
