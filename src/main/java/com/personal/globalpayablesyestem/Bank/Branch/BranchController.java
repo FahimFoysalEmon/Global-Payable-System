ac3a2bb84f2ecaae3b8c79f3ac4996affda50123
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-08-31 16:27:01 +0600

    branch service and branch controller added

---
+++ src/main/java/com/personal/globalpayablesyestem/Bank/Branch/BranchController.java
@@ -0,0 +1,11 @@
+package com.personal.globalpayablesyestem.Bank.Branch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@RequiredArgsConstructor
+public class BranchController {
+}
