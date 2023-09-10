879433b5bd9d1c51992633002254326a0e24fdc5
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-09-10 12:44:04 +0600

    country url modified

--- src/main/java/com/personal/globalpayablesyestem/Country/CountryController.java
+++ src/main/java/com/personal/globalpayablesyestem/Country/CountryController.java
@@ -1,7 +1,52 @@
 package com.personal.globalpayablesyestem.Country;
 
-import org.springframework.web.bind.annotation.RestController;
 
+import com.personal.globalpayablesyestem.Bank.validators.BankIdMustExist;
+import com.personal.globalpayablesyestem.Country.Utils.CountryEndpointUtils;
+import com.personal.globalpayablesyestem.Country.validators.CountryIdMustExist;
+import com.personal.globalpayablesyestem.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/private/admin")
+@Validated
 public class CountryController {
+
+    private final CountryService countryService;
+
+    @GetMapping(value = CountryEndpointUtils.GET_ALL_COUNTRIES)
+    public ResponseEntity<ApiResponse> getCountries() {
+        return new ResponseEntity<>(new ApiResponse("Success",countryService.getCountries(), null), HttpStatus.OK);
+    }
+
+    @PostMapping(value = CountryEndpointUtils.ADD_COUNTRY)
+    public ResponseEntity<ApiResponse> addCountry(@RequestBody Country country){
+        return new ResponseEntity<>(new ApiResponse("Success",countryService.addCountry(country),null), HttpStatus.OK);
+    }
+
+    @GetMapping(value = CountryEndpointUtils.GET_COUNTRY)
+    public ResponseEntity<ApiResponse> getCountry(@PathVariable @CountryIdMustExist String countryId) {
+        return new ResponseEntity<>(new ApiResponse("Success", countryService.getCountry(countryId), null), HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = CountryEndpointUtils.UPDATE_COUNTRY)
+    public ResponseEntity<ApiResponse> updateCountry(@PathVariable @CountryIdMustExist String countryId,
+                                                  @RequestBody Country country) {
+        return new ResponseEntity<>(new ApiResponse("Success", countryService.updateCountry(countryId, country), null), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(value = CountryEndpointUtils.DELETE_COUNTRY)
+    public ResponseEntity<ApiResponse> deleteCountry(@PathVariable @CountryIdMustExist String countryId,
+                                                     @PathVariable @BankIdMustExist String bankId) {
+        countryService.deleteCountry(countryId, bankId);
+        return new ResponseEntity<>(new ApiResponse("Success", null , null), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/personal/globalpayablesyestem/Country/CountryService.java
+++ src/main/java/com/personal/globalpayablesyestem/Country/CountryService.java
@@ -4,4 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CountryService {
+    public Object getCountries() {
+    }
+
+    public Object addCountry(Country country) {
+    }
+
+    public Object getCountry(String countryId) {
+    }
+
+    public Object updateCountry(String countryId, Country country) {
+    }
+
+    public void deleteCountry(String countryId, String bankId) {
+    }
 }
