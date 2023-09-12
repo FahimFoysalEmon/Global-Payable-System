046329e7641192f844385138540b4510aca7b30b
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-09-12 17:26:27 +0600

    acc controller added

---
+++ src/main/java/com/personal/globalpayablesyestem/bank/branch/account/AccountController.java
@@ -0,0 +1,9 @@
+package com.personal.globalpayablesyestem.bank.branch.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class AccountController {
+}
