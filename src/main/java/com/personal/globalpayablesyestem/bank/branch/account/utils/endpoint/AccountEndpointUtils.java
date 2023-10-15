1573a689ecc4b27e7c5ba035d878d316454d75a4
FahimFoysalEmon <fahim.foysal.emon@gmail.com>
2023-10-15 16:34:24 +0600

    acc balance url added for user

--- src/main/java/com/personal/globalpayablesyestem/bank/branch/account/utils/endpoint/AccountEndpointUtils.java
+++ src/main/java/com/personal/globalpayablesyestem/bank/branch/account/utils/endpoint/AccountEndpointUtils.java
@@ -4,4 +4,5 @@ public class AccountEndpointUtils {
 
     public static final String ADD_ACCOUNT = "/bank/{bankId}/branch/{branchId}";
     public static final String BANK_DEPOSIT ="/bank/{bankId}/branch/{branchId}/deposit" ;
+    public static final String GET_BALANCE ="/bank/{bankId}/branch/{branchId}/account/balance" ;
 }
